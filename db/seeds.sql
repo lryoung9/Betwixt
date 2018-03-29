5f83d8c8b5abc56ba15d752be66b5b1ff1dc7e5c
Lydia Young <lydiaryoung@gmail.com>
2018-03-29 11:25:04 -0600

    added table column for users' addresses

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 USE betwixtDB;
 
-INSERT INTO client (name, email, password)
-    VALUES ("lryoung", "lydiaryoung@gmail.com", "lydia");
+INSERT INTO client (name, email, address, password)
+    VALUES ("lryoung", "lydiaryoung@gmail.com", "1100 West Chenango Avenue Englewood, Colorado 80110", "lydia");
